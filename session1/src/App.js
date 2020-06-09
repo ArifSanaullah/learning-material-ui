357767b4430aadee025c8e79510d00fa1dd64168
ArifSanaullah <ak7896023@gmail.com>
2020-06-09 18:29:02 +0500

    initial test commit

--- session1/src/App.js
+++ session1/src/App.js
@@ -1,26 +1,8 @@
 import React from "react";
 import "./App.css";
-import { Grid } from "@material-ui/core";
-import Header from "./Header";
-import Content from "./Content";
 
 function App() {
-  return (
-    <Grid container direction="column">
-      <Grid item>
-        <Header />
-      </Grid>
-      <Grid item container>
-        <Grid xs={12} container>
-          <Grid xs={0} sm={2} item />
-          <Grid xs={12} sm={8} item>
-            <Content />
-          </Grid>
-          <Grid sm={2} xs={0} item />
-        </Grid>
-      </Grid>
-    </Grid>
-  );
+  return <div>Hello world</div>;
 }
 
 export default App;
--- session1/src/index.js
+++ session1/src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "@material-ui/core/styles";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
