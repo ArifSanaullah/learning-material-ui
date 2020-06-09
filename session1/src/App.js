bc88a5e6d85797d4a6bac07282f7835e6ed589c6
ArifSanaullah <ak7896023@gmail.com>
2020-06-09 22:07:57 +0500

    feat: makeStyles

--- session1/src/App.js
+++ session1/src/App.js
@@ -1,8 +1,39 @@
 import React from "react";
 import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Typography } from "@material-ui/core";
+import CoolButton from "./CoolButton";
+import classNames from "classnames";
+
+const useStyles = makeStyles((theme) => ({
+  buttonStyles: {
+    color: "green",
+    border: "1",
+  },
+  textStyles: {
+    color: "red",
+    [theme.breakpoints.up("sm")]: {
+      color: "blue",
+    },
+  },
+  textBackground: {
+    backgroundColor: "gray",
+  },
+}));
 
 function App() {
-  return <div>Hello world</div>;
+  const classes = useStyles();
+  return (
+    <div>
+      <CoolButton cool={true} />
+      <Button className={classes.buttonStyles}>Hello world</Button>
+      <Typography
+        className={classNames(classes.textStyles, classes.textBackground)}
+      >
+        Hello world text
+      </Typography>
+    </div>
+  );
 }
 
 export default App;
