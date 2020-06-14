dbbd9c7e0cf65d3f688f6caa1e11a9b24c0a0a06
ArifSanaullah <ak7896023@gmail.com>
2020-06-14 20:08:39 +0500

    feat: themeing

--- session1/src/App.js
+++ session1/src/App.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { Switch, Grid, Typography, Button, Paper } from "@material-ui/core";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 function App() {
-  return <div>Hello world</div>;
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Paper style={{ height: "100vh" }}>
+        <Grid container direction="column">
+          <Typography variant="h1">Hello World</Typography>
+          <Button variant="contained" color="primary">
+            This is button
+          </Button>
+          <Button variant="contained" color="secondary">
+            This is another button
+          </Button>
+          <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+        </Grid>
+      </Paper>
+    </ThemeProvider>
+  );
 }
 
 export default App;
