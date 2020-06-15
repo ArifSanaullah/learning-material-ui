568a5ffb1d86f0ae929e7b643c0903b5cec434f3
ArifSanaullah <ak7896023@gmail.com>
2020-06-15 08:03:44 +0500

    feat: dummy grid structure

--- session1/src/App.js
+++ session1/src/App.js
@@ -24,12 +24,23 @@ function App() {
     <ThemeProvider theme={createMuiTheme({ palette: { type: "dark" } })}>
       <Paper>
         <div className={classes.root}>
-          <Grid container spacing={3}>
+          <Grid container spacing={3} direction="column">
             <Grid item xs={12}>
               <Paper className={classes.paper}>xs=12</Paper>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <Paper className={classes.paper}>xs=12 sm=6</Paper>
+            <Grid item xs={12} container >
+              <Grid xs={3}>
+                <Paper className={classes.paper}>xs=3</Paper>
+              </Grid>
+              <Grid xs={3}>
+                <Paper className={classes.paper}>xs=3</Paper>
+              </Grid>
+              <Grid xs={3}>
+                <Paper className={classes.paper}>xs=3</Paper>
+              </Grid>
+              <Grid xs={3}>
+                <Paper className={classes.paper}>xs=3</Paper>
+              </Grid>
             </Grid>
             <Grid item xs={12} sm={6}>
               <Paper className={classes.paper}>xs=12 sm=6</Paper>
