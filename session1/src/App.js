e9ebb1b124be1fb0f6e0e6903aaaecd3693b9c1b
ArifSanaullah <ak7896023@gmail.com>
2020-06-15 07:47:18 +0500

    initial commit

--- session1/src/App.js
+++ session1/src/App.js
@@ -1,8 +1,56 @@
 import React from "react";
-import "./App.css";
+import {
+  makeStyles,
+  ThemeProvider,
+  createMuiTheme,
+} from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+  },
+}));
 
 function App() {
-  return <div>Hello world</div>;
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={createMuiTheme({ palette: { type: "dark" } })}>
+      <Paper>
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>xs=12</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paper}>xs=12 sm=6</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Paper className={classes.paper}>xs=12 sm=6</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>xs=6 sm=3</Paper>
+            </Grid>
+            <Grid item xs={6} sm={3}>
+              <Paper className={classes.paper}>xs=6 sm=3</Paper>
+            </Grid>
+          </Grid>
+        </div>
+      </Paper>
+    </ThemeProvider>
+  );
 }
 
 export default App;
